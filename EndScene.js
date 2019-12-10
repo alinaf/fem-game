63068bfefc01518a78970557f61590dfe0b7ddbd
Alina Abidi <aabidi@usc.edu>
2019-12-10 12:57:15 -0800

    bare bones game play

--- EndScene.js
+++ EndScene.js
@@ -4,7 +4,7 @@ class EndScene extends Phaser.Scene {
 	}
 
 	create() {
-		this.add.text( 150, 250, 'Click to start!', {fill: '#000000', fontSize: '20px'})
+		this.add.text( 150, 250, 'The End!', {fill: '#000000', fontSize: '20px'})
 		this.input.on('pointerdown', () => {
 			// this.scene.stop('EndScene')
 			// this.scene.start('GameScene')
--- GameScene.js
+++ GameScene.js
@@ -1,55 +1,74 @@
 var counter = 0;
+var word;
+var context;
 
 class GameScene extends Phaser.Scene {
 
-	constructor(){
-		super({ key: 'GameScene' })
-	}
+    constructor() {
+        super({
+            key: 'GameScene'
+        })
+    }
 
-	preload() {
-		this.load.image('select', 'assets/select.png');
-	}
+    preload() {
+        this.load.image('select', 'assets/select.png');
+    }
 
+    create() {
+        context = this;
+        this.cameras.main.backgroundColor.setTo(255, 255, 254);
 
-	create() {
-		this.cameras.main.backgroundColor.setTo(255,255,254);
+        word = this.add.text(w / 2, h / 2 - h / 4, answers[counter].word, {
+            fill: colors.headline,
+            font: 'bold 80px Montserrat'
+        }).setOrigin(0.5, 0.5);
 
-		const word = this.add.text(w/2, h/2 - h/4, answers[0].word, {fill: colors.headline, font: 'bold 80px Montserrat'}).setOrigin(0.5, 0.5);
-		
-		// buttons
-		const fem = this.add.sprite(w/4 - 50, 2 * h/3, 'select');
-    	fem.setOrigin(0.5);
-    	fem.setInteractive();
-    	fem.on('pointerup', function (pointer) {
-    	});
-    	fem.tint = colors.button;
-    	this.add.text(fem.x, fem.y, "fem", {fill: colors.headline, font: '50px Montserrat'}).setOrigin(0.5, 0.5);
+        // buttons
+        const fem = this.add.sprite(w / 4 - 50, 2 * h / 3, 'select');
+        fem.setOrigin(0.5);
+        fem.setInteractive();
+        fem.on('pointerup', function(pointer) {
+            choose('f');
+        });
+        fem.tint = colors.button;
+        this.add.text(fem.x, fem.y, "fem", {
+            fill: colors.headline,
+            font: '50px Montserrat'
+        }).setOrigin(0.5, 0.5);
 
-    	const masc = this.add.sprite(w/2, 2 * h/3, 'select');
-    	masc.setOrigin(0.5);
-    	masc.setInteractive();
-    	masc.on('pointerup', function (pointer) {
-    	});
-    	masc.tint = colors.button;
-    	this.add.text(masc.x, masc.y, "masc", {fill: colors.headline, font: '50px Montserrat'}).setOrigin(0.5, 0.5);
+        const masc = this.add.sprite(w / 2, 2 * h / 3, 'select');
+        masc.setOrigin(0.5);
+        masc.setInteractive();
+        masc.on('pointerup', function(pointer) {
+            choose('m');
+        });
+        masc.tint = colors.button;
+        this.add.text(masc.x, masc.y, "masc", {
+            fill: colors.headline,
+            font: '50px Montserrat'
+        }).setOrigin(0.5, 0.5);
 
-    	const neutral = this.add.sprite(3 * w/4 + 50, 2 * h/3, 'select');
-    	neutral.setOrigin(0.5);
-    	neutral.setInteractive();
-    	neutral.on('pointerup', function (pointer) {
-    	});
-    	neutral.tint = colors.button;
-    	this.add.text(neutral.x, neutral.y, "neutral", {fill: colors.headline, font: '50px Montserrat'}).setOrigin(0.5, 0.5);
+        const neutral = this.add.sprite(3 * w / 4 + 50, 2 * h / 3, 'select');
+        neutral.setOrigin(0.5);
+        neutral.setInteractive();
+        neutral.on('pointerup', function(pointer) {
+            choose('n');
+        });
+        neutral.tint = colors.button;
+        this.add.text(neutral.x, neutral.y, "neutral", {
+            fill: colors.headline,
+            font: '50px Montserrat'
+        }).setOrigin(0.5, 0.5);
 
-	}
+    }
+}
 
-	update() {
-		// if (gameState.cursors.left.isDown) {
-		// 	gameState.player.setVelocityX(-160);
-		// } else if (gameState.cursors.right.isDown) {
-		// 	gameState.player.setVelocityX(160);
-		// } else {
-		// 	gameState.player.setVelocityX(0);
-		// }
-	}
+function choose(choice) {
+    word.setText(answers[counter].word);
+    console.log(choice, counter);
+    counter++;
+    if (counter == answers.length) {
+    	context.scene.stop('GameScene')
+		context.scene.start('EndScene')
+    }
 }
